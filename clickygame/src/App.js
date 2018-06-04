1cd322541c813fd5fa128f42a6ab1c0e71b4a824
Thomas Seaman <thomasseaman@Thomas-School-MacBook-Pro.local>
2018-06-03 22:14:45 -0700

    push

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -11,18 +11,23 @@ class App extends Component {
     friends
   };
 
-  removeFriend = id => {
+
+removeFriend = id => {
     // Filter this.state.friends for friends with an id not equal to the id being removed
-    const friends = this.state.friends.filter(friend => friend.id !== id);
+    // const friends = this.state.friends.filter(friend => friend.id !== id);
     // Set this.state.friends equal to the new friends array
-    this.setState({ friends });
-  };
+    // this.setState({ friends });
+    
+    let chosenSports = []
+    chosenSports.push(id)
+    console.log(chosenSports)
+};
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
       <Wrapper>
-        <Title>Friends List</Title>
+        <Title>Sports Memory Challenge</Title>
         {this.state.friends.map(friend => (
           <FriendCard
             removeFriend={this.removeFriend}
--- clickygame/src/components/FriendCard/FriendCard.js
+++ clickygame/src/components/FriendCard/FriendCard.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./FriendCard.css";
 
+let chosenSports = []
+
 const FriendCard = props => (
- 
+
     <div className="img-container">
+    <button onClick={() => props.removeFriend(props.id)} className="remove">
       <img alt={props.name} src={props.image} value={props.id} href={props.id}/>
+    </button>
     </div>
   
 );
