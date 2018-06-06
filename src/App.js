94f342d987c222c343ce9bbf2b0dc35de8ead49d
Thomas Seaman <thomasseaman@Thomas-School-MacBook-Pro.local>
2018-06-05 21:35:55 -0700

    debugged and readme complete

--- src/App.js
+++ src/App.js
@@ -3,10 +3,8 @@ import FriendCard from "./components/FriendCard";
 import Title from "./components/Title";
 import Score from "./components/Score";
 import friends from "./friends.json";
-import "./App.css";
 
 class App extends Component {
-  // Setting this.state.friends to the friends json array
   state = {
     friends,
     chosenSports: [],
@@ -14,96 +12,67 @@ class App extends Component {
     highScore: 0
   };
 
+  scoreKeeper = () => {
 
-scoreKeeper = () => {
-
-  if(this.state.score >= this.state.highScore){
-    this.setState({
-      highScore: this.state.highScore +1,
-      score: this.state.score +1
-    })
-    console.log('if'); 
-  }if(this.state.score == 11){
-    alert("Winner! What a memory you have.")
-    this.resetGame()
-  } 
-  else {
-    this.setState({score: this.state.score +1 })
-    console.log('else');
+    if (this.state.score >= this.state.highScore) {
+      this.setState({
+        highScore: this.state.highScore + 1,
+        score: this.state.score + 1
+      })
+    } if (this.state.score === 11) {
+      alert("Winner! What a memory you have.")
+      this.resetGame()
+    }
+    else {
+      this.setState({ score: this.state.score + 1 })
+    }
   }
-}  
 
-
-resetGame = () => {
-  this.setState({score:0})
-  this.setState({chosenSports:[]})
-}
-
-shuffle = id => {
-  // Shuffle the array
-  for (let i = friends.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [friends[i], friends[j]] = [friends[j], friends[i]];
+  resetGame = () => {
+    this.setState({ score: 0 })
+    this.setState({ chosenSports: [] })
   }
-  // Update state with new array
-  this.setState({ friends });
-}
-// removeFriend = id => {
-//     // Filter this.state.friends for friends with an id not equal to the id being removed
-//     // const friends = this.state.friends.filter(friend => friend.id !== id);
-//     // Set this.state.friends equal to the new friends array
-//     // this.setState({ friends });
-//     let chosenSports = []
-//     console.log(chosenSports)
-// };
 
-imageClickHandler = event => {
-  
-  const { id, alt } = event.target
+  shuffle = id => {
+    for (let i = friends.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [friends[i], friends[j]] = [friends[j], friends[i]];
+    }
+    this.setState({ friends });
+  }
 
-  this.state.chosenSports.push(id)
-  this.scoreKeeper()
-  this.shuffle()
-  this.hasDuplicates()
-};
+  imageClickHandler = event => {
 
-hasDuplicates = (array)=> {
-  
-  const sorted_arr = this.state.chosenSports.slice().sort();
-  console.log(sorted_arr)
-  
-}
-// logMe(){
-//   console.log(this.state);
-  
-// }
-// programHandler = (event) => {
-//   //console.log(event.target.value)
-//   this.setState({program: event.target.value})
-// }
+    const { id } = event.target
 
-  // Map over this.state.friends and render a FriendCard component for each friend object
+    if (this.state.chosenSports.indexOf(id) === -1) {
+      this.state.chosenSports.push(id)
+      this.scoreKeeper()
+      this.shuffle()
+    } else {
+      alert("you lose...")
+      this.resetGame()
+    }
+  };
+
   render() {
     return (
       <div className="row">
         <Title></Title>
         <Score score={this.state.score} highScore={this.state.highScore}></Score>
-      <div className="row">
-        {this.state.friends.map(friend => (
-          <FriendCard
-            imageClickHandler={this.imageClickHandler}
-            id={friend.id}
-            key={friend.id}
-            name={friend.name}
-            image={friend.image}
-          />
-        ))}
+        <div className="row">
+          {this.state.friends.map(friend => (
+            <FriendCard
+              imageClickHandler={this.imageClickHandler}
+              id={friend.id}
+              key={friend.id}
+              name={friend.name}
+              image={friend.image}
+            />
+          ))}
         </div>
-        {/* <button onClick={() => this.logMe()}>Log Me</button> */}
-        {/* <button onClick={this.logMe.bind(this)}>Log Me</button> */}
       </div>
     );
   }
-
 }
 export default App;
--- src/components/FriendCard/FriendCard.js
+++ src/components/FriendCard/FriendCard.js
@@ -4,14 +4,10 @@ import "./FriendCard.css";
 
 const FriendCard = props => (
 
-    <div className="img-container col-2">
-    {/* <button onClick={(selectedHandler)}> */}
-      {/* <img onClick={() => props.imageClickHandler(props.id)} alt={props.name} src={props.image} value={props.id}/> */}
-      <img onClick={props.imageClickHandler} id={props.id} alt={props.name} src={props.image}/>
+  <div className="img-container col-2">
+    <img onClick={props.imageClickHandler} id={props.id} alt={props.name} src={props.image} />
+  </div>
 
-    {/* </button> */}
-     </div>
-  
 );
 
 export default FriendCard;
